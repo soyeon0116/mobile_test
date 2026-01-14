167cf0c87cb2f102dda5db30f25a4ca03beb5f6b
soyeon <ksy9252@naver.com>
2026-01-14 14:56:06 +0900

    feat : base 추가

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/mobile_test/",
   plugins: [react()],server: { port: 3000, host: '0.0.0.0', },
 })
